73034ce31fbb4e55c6aada90569e9d5389e5f6ab
Rsda23 <rsda23.0@gmail.com>
2025-03-02 19:28:54 +0100

    chore: add version 1.0.3 domain package

--- Models/Exercice.cs
+++ Models/Exercice.cs
@@ -1,13 +1,22 @@
-﻿namespace GymProgress.Domain.Models
+﻿using System.Text.Json.Serialization;
+
+namespace GymProgress.Domain.Models
 {
     public class Exercice
     {
+        [JsonPropertyName("exerciceId")]
         public string ExerciceId { get; set; } = string.Empty;
+        [JsonPropertyName("name")]
         public string Nom { get; set; }
+        [JsonPropertyName("repetition")]
         public int Repetition { get; set; }
+        [JsonPropertyName("serie")]
         public int Serie { get; set; }
+        [JsonPropertyName("charge")]
         public float Charge { get; set; }
+        [JsonPropertyName("date")]
         public DateTime Date { get; set; }
+        [JsonPropertyName("userId")]
         public string UserId { get; set; }
 
         public Exercice()
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,16 @@
-﻿namespace GymProgress.Domain.Models
+﻿using System.Text.Json.Serialization;
+
+namespace GymProgress.Domain.Models
 {
     public class User
     {
+        [JsonPropertyName("userId")]
         public string UserId { get; set; }
+        [JsonPropertyName("pseudo")]
         public string Pseudo { get; set; }
+        [JsonPropertyName("email")]
         public string Email { get; set; }
+        [JsonPropertyName("hashedPassword")]
         public string HashedPassword { get; set; }
 
         public User()
